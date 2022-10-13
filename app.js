f78736b27f02d83f6c098b41cbc21bfaa008cbd0
Irene Panis <panisirene@gmail.com>
2022-10-13 03:10:20 -0700

    Add functionality to most buttons

--- app.js
+++ app.js
@@ -22,3 +22,40 @@ function operate(operator, a, b) {
     case divide: return divide(a, b); break;
   }
 }
+
+function percentage(a, b) {
+  let percent = a / 100;
+  return percent * b;
+}
+
+const buttons = document.querySelectorAll('.disp');
+const input = document.querySelector('#input-box');
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+const equals = document.querySelector('#equals');
+let displayValue;
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    input.innerHTML += button.textContent;
+    displayValue = input.innerHTML;
+  });
+});
+
+clear.addEventListener('click', () => {
+  input.innerHTML = '';
+});
+
+backspace.addEventListener('click', () => {
+  if (input.innerHTML !== '') {
+    input.innerHTML = displayValue.slice(0, -1);
+    displayValue = input.innerHTML;
+  };
+});
+
+equals.addEventListener('click', () => {
+  let array = displayValue.split(/(\D)/g);
+  array.reduce((a, b) => {
+
+  });
+});

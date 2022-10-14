66ebae2650d92f414dc78ac316f242df9f9b3d00
Irene Panis <panisirene@gmail.com>
2022-10-14 01:21:07 -0700

    Add functionality to equals button

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 function add(a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
   return a + b;
 }
 
 function subtract(a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
   return a - b;
 }
 
 function multiply(a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
   return a * b;
 }
 
 function divide(a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
   return a / b;
 }
 
 function operate(operator, a, b) {
   switch (operator) {
-    case add: return add(a, b); break;
-    case subtract: return subtract(a, b); break;
-    case multiply: return multiply(a, b); break;
-    case divide: return divide(a, b); break;
+    case "add": return add(a, b); break;
+    case "subtract": return subtract(a, b); break;
+    case "multiply": return multiply(a, b); break;
+    case "divide": return divide(a, b); break;
+    case "percentage": return percentage(a, b); break;
   }
 }
 
@@ -29,21 +38,56 @@ function percentage(a, b) {
 }
 
 const buttons = document.querySelectorAll('.disp');
+const operators = document.querySelectorAll('.oper');
 const input = document.querySelector('#input-box');
+const answer = document.querySelector('#answer');
 const clear = document.querySelector('#clear');
 const backspace = document.querySelector('#backspace');
 const equals = document.querySelector('#equals');
 let displayValue;
+let firstNumber;
+let operatorChosen;
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    input.innerHTML += button.textContent;
-    displayValue = input.innerHTML;
+    answer.innerHTML += button.textContent;
+    displayValue = answer.innerHTML;
   });
 });
 
+operators.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (input.innerHTML === '') {
+      answer.innerHTML = '';
+      input.innerHTML = displayValue;
+      firstNumber = convertDisplay(displayValue);
+      operatorChosen = button.id;
+    } else {
+      answer.innerHTML = operate(operatorChosen, firstNumber, convertDisplay(displayValue));
+      firstNumber = answer.innerHTML;
+      input.innerHTML = '';
+    }
+  });
+});
+
+function convertOperator(operator) {
+  switch(operator) {
+    case "add": return "+"; break;
+    case "subtract": return "-"; break;
+    case "multiply": return "×"; break;
+    case "divide": return "÷"; break;
+    case "percentage": return "%"; break;
+  }
+}
+
+function convertDisplay(str) {
+  return parseFloat(str.split(/\D/)[0]);
+}
+
 clear.addEventListener('click', () => {
-  input.innerHTML = '';
+  clearDisplayValue();
+  firstNumber = null;
+  operatorChosen = null;
 });
 
 backspace.addEventListener('click', () => {
@@ -54,8 +98,14 @@ backspace.addEventListener('click', () => {
 });
 
 equals.addEventListener('click', () => {
-  let array = displayValue.split(/(\D)/g);
-  array.reduce((a, b) => {
-
-  });
+  answer.innerHTML = '';
+  input.innerHTML = '';
+  let secondNumber = convertDisplay(displayValue);
+  answer.innerHTML = operate(operatorChosen, firstNumber, secondNumber);
 });
+
+function clearDisplayValue() {
+  displayValue = '';
+  input.innerHTML = '';
+  answer.innerHTML = '';
+}
